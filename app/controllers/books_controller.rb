6f9903f45b1f00c4e82d9ddd7976eeea36e220de
Natalia <natalixskowronek@gmail.com>
2018-06-20 21:22:53 +0200

    form field with autowrite

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,14 @@ class BooksController < ApplicationController
   before_action :load_book, only: :show
   before_action :new_book, only: :create
 
-  def index
-  end
+  # def index
+  # end
 
-  def new
-  end
+  # def new
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def create
     if new_book.save
@@ -23,11 +23,22 @@ class BooksController < ApplicationController
   def show
   end
 
-  def destroy
+  def filter
+		render template: 'books/filter', locals: { books: filter_books }
   end
 
   private
 
+	def filter_arams
+		permitted_params.slice(:title, :isbn)
+										.merge(category.present? ? { category_id: category.id } : {})
+										.reject{ |k, v| v.to_s.empty? }
+	end
+	
+	def filter_books
+		Book.where(filter_params)
+	end
+	
   def load_books
     @books = Book.all
   end
@@ -36,7 +47,15 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+	def category 
+		Category.find_by(name: permitted_params[:category_name])
+	end
+	
   def new_book
     @book = Book.new(title: params[:title], isbn: params[:isbn], category_name: params[:category_name])
   end
+	
+	def permitted_params
+		params.permit(:title, :isbn, :category_id, :category_name)
+	end
 end
